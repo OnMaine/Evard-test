c1d74908e8c6303a6201bb5a1df0cda70e753318
Володимир <onmaine@gmail.com>
2020-07-17 22:42:46 +0300

    wawes

--- js/index.js
+++ js/index.js
@@ -1,35 +1,35 @@
-// let canvas = document.querySelector("canvas");
-// let ctx = canvas.getContext("2d");
-//
-// function draw() {
-//   let width = window.innerWidth;
-//   let height = 185;
-//   canvas.width = width;
-//   canvas.height = height;
-//
-//   ctx.clearRect(0, 0, width, height);
-//
-//   ctx.fillStyle = "#FFFFFF";
-//
-//   ctx.beginPath();
-//   ctx.moveTo(0, height / 2);
-//
-//
-//   for (let x = 0, amplitude = 4; x < width; x++) {
-//     let y = Math.sin(x / amplitude * Math.PI / 15) * amplitude;
-//     ctx.lineTo(x, y + height / 2);
-//   }
-//
-//   ctx.lineTo(width, height);
-//   ctx.lineTo(0, height);
-//   ctx.lineTo(0, height / 2);
-//
-//   ctx.fill();
-// }
-//
-// draw();
-//
-// window.addEventListener("resize", draw);
+let canvas = document.querySelector("canvas");
+let ctx = canvas.getContext("2d");
+
+function draw() {
+  let width = window.innerWidth;
+  let height = 345;
+  canvas.width = width;
+  canvas.height = height;
+
+  ctx.clearRect(0, 0, width, height);
+
+  ctx.fillStyle = "#FFFFFF";
+
+  ctx.beginPath();
+  ctx.moveTo(0, height / 2);
+
+
+  for (let x = 0, amplitude = 4; x < width; x++) {
+    let y = Math.sin(x / amplitude * Math.PI / 15) * amplitude;
+    ctx.lineTo(x, y + height / 2);
+  }
+
+  ctx.lineTo(width, height);
+  ctx.lineTo(0, height);
+  ctx.lineTo(0, height / 2);
+
+  ctx.fill();
+}
+
+draw();
+
+window.addEventListener("resize", draw);
 
 // amount
 let amount = 10;
